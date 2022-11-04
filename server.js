be3cca5c64ce03c8f6d0a1057184138898a7be6c
Evince Edouard <studioimac@evinces-imac.mynetworksettings.com>
2022-11-04 19:25:16 -0400

    add packages

--- server.js
+++ server.js
@@ -1,22 +1,50 @@
 //DEPENDENCIES
 const express = require('express');
-const exphbs = require('express-handlebars');
 const path = require('path');
-const hbs = exphbs.create({});
+//Import Express Session
+const session = require('express-session')
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const hbs = exphbs.create({});
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+
 
 //Express App
 const app = express();
 const PORT =  process.env.PORT || 3001;
+//Set up sessions
+const sess = {
+    secret: '',
+    cookie: {},
+    resave: false, 
+    saveUninitializaed: false,
+    store: new SequelizeStore({
+        db: sequelize
+    }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
 
 // Set Handlebars as the default template engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 app.use(Express.static(path.join(__dirname, 'public')));
-//Route setup
-app.use(require('./controllers/dish-routes'));
 
+//Route setup ///add route
+app.use(require('./controllers/'));
+
+//Connect to database before starting express.js server
 //Starts server
-app.listen(PORT, () => {
-    console.log('Server listening on: http://localhost:' + PORT);
-})
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Server listening on: http://localhost:' + PORT));
+});
+
+
